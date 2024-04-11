6b9a145f70da33cc94b8f3c02e2bf4ba07053a40
Suraj Singh <suraj.singh080401@gmail.com>
2024-04-11 13:47:56 +0530

    chore : Adding supabase configuration and keys

---
+++ components/App.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { useEffect, useState } from "react";
+import { fetchData } from "../utils/services/userData";
+
+const App = () => {
+	const [data, setData] = useState<any[]>([]);
+
+	useEffect(() => {
+		const fetchDataFromService = async () => {
+			try {
+				const fetchedData = await fetchData();
+				console.log(fetchedData);
+				setData(fetchedData);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		fetchDataFromService();
+	}, []);
+
+	return (
+		<div>
+			<h2>Data from Supabase</h2>
+			<ul>
+				{data.map((item: any) => (
+					<li key={item.id}>{item.title}</li>
+				))}
+			</ul>
+		</div>
+	);
+};
+
+export default App;
---
+++ utils/services/userData.ts
@@ -0,0 +1,15 @@
+import supabase from "../supabaseClient";
+
+export async function fetchData() {
+	try {
+		const { data, error } = await supabase.from("user").select("*");
+		if (error) {
+			throw error;
+		}
+		console.log(data);
+		return data || [];
+	} catch (error: any) {
+		console.error("Error fetching data:", error.message);
+		throw error;
+	}
+}
