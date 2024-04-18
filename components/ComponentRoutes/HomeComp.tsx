7b4cf58ae21feeb9d34ec86c6d7fff994a9c87c0
Suraj Singh <suraj.singh080401@gmail.com>
2024-04-18 18:43:28 +0530

    feat : Adding popular category section

--- components/ComponentRoutes/HomeComp.tsx
+++ components/ComponentRoutes/HomeComp.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Jumbotron from "../Jumbotron";
+import PopularJobs from "../PopularJobs";
 const HomeComp: React.FC = () => {
 	return (
 		<div className="h-full w-full flex flex-col">
 			<Jumbotron />
+			<PopularJobs />
 		</div>
 	);
 };
---
+++ components/PopularJobs.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import PopularJobCard from "./PopularJobCard";
+import {
+	PiCodeBlockThin,
+	PiAndroidLogoThin,
+	PiDatabaseThin,
+	PiGraphThin,
+	PiUserFocusThin,
+	PiCodeLight,
+} from "react-icons/pi";
+
+const PopularJobs: React.FC = () => {
+	return (
+		<div className="flex flex-col md:p-16 p-8 md:space-y-16 space-y-8">
+			<div className="flex flex-col space-y-4">
+				<div className="text-center">
+					<p className="md:text-3xl text-xl">Popular Job Categories</p>
+				</div>
+				<div className="text-center">
+					<p className="text-xs text-gray-500">
+						2072 jobs live - 293 added today.
+					</p>
+				</div>
+			</div>
+			<div className="grid md:grid-cols-2 grid-cols-2 lg:grid-cols-3 grid-flow-row md:gap-8 gap-2">
+				<PopularJobCard
+					title={"Website Developer"}
+					logo={PiCodeBlockThin}
+					count={23}
+				/>
+				<PopularJobCard
+					title={"Android Developer"}
+					logo={PiAndroidLogoThin}
+					count={12}
+				/>
+				<PopularJobCard
+					title={"Database Developer"}
+					logo={PiDatabaseThin}
+					count={43}
+				/>
+				<PopularJobCard title={"Data Analyst"} logo={PiGraphThin} count={14} />
+				<PopularJobCard
+					title={"Software General"}
+					logo={PiCodeLight}
+					count={16}
+				/>
+				<PopularJobCard
+					title={"Manager Profile"}
+					logo={PiUserFocusThin}
+					count={11}
+				/>
+			</div>
+		</div>
+	);
+};
+
+export default PopularJobs;
